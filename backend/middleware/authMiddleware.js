4acd9e296ef86a51ab5f8469648c618f4d90bab1
Kuldeep Jain <kuldipjain2002@gmail.com>
2023-07-28 11:28:18 +0530

    authorization

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModel.js");
+const asyncHandler = require("express-async-handler");
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      // console.log(decoded);
+      req.user = await User.findById(decoded.id).select(" -password"); //dont send the password
+      next();
+    } catch (err) {
+      res.status(401);
+      throw new Error("Not authorised , token failed");
+    }
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized, no token");
+  }
+});
+module.exports = { protect };
